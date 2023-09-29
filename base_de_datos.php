cab297a2eec0dc8c52938db18b2bbfcf2b6229c4
herlita <herlita.farro@tecsup.edu.pe>
2023-09-28 22:47:42 -0500

    practica1

--- base_de_datos.php
+++ base_de_datos.php
@@ -6,7 +6,7 @@ $rutaServidor = "containers-us-west-105.railway.app";
 $puerto = "7795";
 try {
  $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
- $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   
+ $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e){
     echo "Ocurrio un error con la base de datos: " . $e->getMessage();
 }
