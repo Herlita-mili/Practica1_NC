4b04eec59c96eb03bec2b510dbfea29c97f1f62b
Herlita Farro <115386147+Herlita-mili@users.noreply.github.com>
2023-09-28 23:31:42 -0500

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -13,8 +13,8 @@
                 <label for="edad">Edad</label>
                 <input required name="edad" type="number" id="edad" placeholder="Edad de mascota" class="form-control">
             </div>
-            <button type="submit" class="btn btn-success">Guardar</button>
-            <a href="./listar.php" class="btn btn-warning">Ver todas</a>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+            <a href="./listar.php" class="btn btn-success">Ver todas</a>
         </form>
     </div>
 </div>
